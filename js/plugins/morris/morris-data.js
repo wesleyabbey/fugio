64fbcb25fdb1fcfc43701781705e4c20d0cb364e
wesleyabbey <wesleyabbey.io@gmail.com>
2015-10-11 05:45:25 -0400

    Added functionality for withdrawals and deposits. Now all I have to do is create forms for post requests

--- js/plugins/morris/morris-data.js
+++ js/plugins/morris/morris-data.js
@@ -1,74 +1,6 @@
 // Morris.js Charts sample data for SB Admin template
 $(document).ready(function() {
 
-    // Bar Chart Withdrawals
-    var myDepositBar = Morris.Bar({
-        element: 'morris-bar-chart-deposit',
-        data: [{
-            day: 'Sun',
-            total: 157
-        }, {
-            day: 'Mon',
-            total: 13
-        }, {
-            day: 'Tue',
-            total: 13
-        }, {
-            day: 'Wed',
-            total: 27
-        }, {
-            day: 'Thu',
-            total: 38
-        }, {
-            day: 'Fri',
-            total: 65
-        }, {
-            day: 'Sat',
-            total: 157
-        }],
-        xkey: 'day',
-        ykeys: ['total'],
-        labels: ['Total Amount In Purchases'],
-        xLabelAngle: 35,
-        hideHover: 'auto',
-        barColors: ['red'],
-        resize: true
-    });
-
-    // Bar Chart Withdrawals
-    var myWithdrawBar = Morris.Bar({
-        element: 'morris-bar-chart-withdraw',
-        data: [{
-            day: 'Sun',
-            total: 157
-        }, {
-            day: 'Mon',
-            total: 13
-        }, {
-            day: 'Tue',
-            total: 13
-        }, {
-            day: 'Wed',
-            total: 27
-        }, {
-            day: 'Thu',
-            total: 38
-        }, {
-            day: 'Fri',
-            total: 65
-        }, {
-            day: 'Sat',
-            total: 157
-        }],
-        xkey: 'day',
-        ykeys: ['total'],
-        labels: ['Total Amount In Purchases'],
-        xLabelAngle: 35,
-        hideHover: 'auto',
-        barColors: ['green'],
-        resize: true
-    });
-
     // Bar Chart Purchases
     var myBar = Morris.Bar({
         element: 'morris-bar-chart',
@@ -159,6 +91,7 @@ $(document).ready(function() {
         total: 1
     }];
 
+    // Get Purchases
     $.get("http://api.reimaginebanking.com/accounts/560f0207f8d8770df0efa563/purchases?key=18cdc5d0c87e95621c50a23782e41559", function(data) {
         // Return a json object containing all the appropriate purchases
         // loop through each purchase and append amounts to the labels
@@ -242,9 +175,7 @@ $(document).ready(function() {
         }, {
             day: weeklyTotals[6]["day"],
             total: weeklyTotals[6]["total"]
-        }]
-
-            );
+        }]);
 
         myDonut.setData([{
             label: '$' + categoryTotals[0]["label"],
@@ -261,178 +192,397 @@ $(document).ready(function() {
         }]);
     });
 
-    // Area Chart
-    Morris.Area({
-        element: 'morris-area-chart',
+
+    var myWithdrawBar = Morris.Bar({
+        element: 'morris-bar-chart-withdraw',
         data: [{
-            period: '2010 Q1',
-            iphone: 2666,
-            ipad: null,
-            itouch: 2647
+            day: 'Sun',
+            total: 1
         }, {
-            period: '2010 Q2',
-            iphone: 2778,
-            ipad: 2294,
-            itouch: 2441
+            day: 'Mon',
+            total: 1
         }, {
-            period: '2010 Q3',
-            iphone: 4912,
-            ipad: 1969,
-            itouch: 2501
+            day: 'Tue',
+            total: 1
         }, {
-            period: '2010 Q4',
-            iphone: 3767,
-            ipad: 3597,
-            itouch: 5689
+            day: 'Wed',
+            total: 1
         }, {
-            period: '2011 Q1',
-            iphone: 6810,
-            ipad: 1914,
-            itouch: 2293
+            day: 'Thu',
+            total: 1
         }, {
-            period: '2011 Q2',
-            iphone: 5670,
-            ipad: 4293,
-            itouch: 1881
+            day: 'Fri',
+            total: 1
         }, {
-            period: '2011 Q3',
-            iphone: 4820,
-            ipad: 3795,
-            itouch: 1588
-        }, {
-            period: '2011 Q4',
-            iphone: 15073,
-            ipad: 5967,
-            itouch: 5175
-        }, {
-            period: '2012 Q1',
-            iphone: 10687,
-            ipad: 4460,
-            itouch: 2028
-        }, {
-            period: '2012 Q2',
-            iphone: 8432,
-            ipad: 5713,
-            itouch: 1791
+            day: 'Sat',
+            total: 1
         }],
-        xkey: 'period',
-        ykeys: ['iphone', 'ipad', 'itouch'],
-        labels: ['iPhone', 'iPad', 'iPod Touch'],
-        pointSize: 2,
+        xkey: 'day',
+        ykeys: ['total'],
+        labels: ['Total Amount In Purchases'],
+        xLabelAngle: 35,
         hideHover: 'auto',
+        barColors: ['red'],
         resize: true
     });
 
-    // Line Chart
-    Morris.Line({
-        // ID of the element in which to draw the chart.
-        element: 'morris-line-chart',
-        // Chart data records -- each entry in this array corresponds to a point on
-        // the chart.
+    var weeklyTotalsWithdrawals = [{
+        day: 'Sun',
+        total: 1
+    }, {
+        day: 'Mon',
+        total: 1
+    }, {
+        day: 'Tue',
+        total: 1
+    }, {
+        day: 'Wed',
+        total: 1
+    }, {
+        day: 'Thu',
+        total: 1
+    }, {
+        day: 'Fri',
+        total: 1
+    }, {
+        day: 'Sat',
+        total: 1
+    }];
+
+    // Get withdrawals
+    $.get("http://api.reimaginebanking.com/accounts/560f0207f8d8770df0efa563/withdrawals?key=18cdc5d0c87e95621c50a23782e41559", function(dat) { 
+        var length = dat.length;
+        $(".wcount").text(length);
+
+        var j = 0;
+        for (var i = dat.length - 1; j < 8; i--) {
+            var date = dat[i]["transaction_date"].substring(0, 10);
+            var time = dat[i]["transaction_date"].substring(11, 19);
+            var amount = dat[i]["amount"];
+            var line = "<tr><td>" + date + "</td><td>" + time + "</td><td>" + amount + "</td></tr>"
+            $(".panel-with tbody").append(line);
+            j++;
+        }
+
+        for (var i = 0; i < length; i++) {
+            var purchase = dat[i];
+
+            var currentDate = new Date(purchase['transaction_date'].substring(0, 19));
+            var day = currentDate.getDay();
+            weeklyTotalsWithdrawals[day]["total"] += purchase["amount"];
+        }
+
+        myWithdrawBar.setData([{
+            day: weeklyTotalsWithdrawals[0]["day"],
+            total: weeklyTotalsWithdrawals[0]["total"]
+        }, {
+            day: weeklyTotalsWithdrawals[1]["day"],
+            total: weeklyTotalsWithdrawals[1]["total"]
+        }, {
+            day: weeklyTotalsWithdrawals[2]["day"],
+            total: weeklyTotalsWithdrawals[2]["total"]
+        }, {
+            day: weeklyTotalsWithdrawals[3]["day"],
+            total: weeklyTotalsWithdrawals[3]["total"]
+        }, {
+            day: weeklyTotalsWithdrawals[4]["day"],
+            total: weeklyTotalsWithdrawals[4]["total"]
+        }, {
+            day: weeklyTotalsWithdrawals[5]["day"],
+            total: weeklyTotalsWithdrawals[5]["total"]
+        }, {
+            day: weeklyTotalsWithdrawals[6]["day"],
+            total: weeklyTotalsWithdrawals[6]["total"]
+        }]);
+    });
+
+    var weeklyTotalsDeposits = [{
+        day: 'Sun',
+        total: 1
+    }, {
+        day: 'Mon',
+        total: 1
+    }, {
+        day: 'Tue',
+        total: 1
+    }, {
+        day: 'Wed',
+        total: 1
+    }, {
+        day: 'Thu',
+        total: 1
+    }, {
+        day: 'Fri',
+        total: 1
+    }, {
+        day: 'Sat',
+        total: 1
+    }];
+
+
+    // Bar Chart Withdrawals
+    var myDepositBar = Morris.Bar({
+        element: 'morris-bar-chart-deposit',
         data: [{
-            d: '2012-10-01',
-            visits: 802
+            day: 'Sun',
+            total: 1
         }, {
-            d: '2012-10-02',
-            visits: 783
+            day: 'Mon',
+            total: 2
         }, {
-            d: '2012-10-03',
-            visits: 820
+            day: 'Tue',
+            total: 3
         }, {
-            d: '2012-10-04',
-            visits: 839
+            day: 'Wed',
+            total: 6
         }, {
-            d: '2012-10-05',
-            visits: 792
+            day: 'Thu',
+            total: 2
         }, {
-            d: '2012-10-06',
-            visits: 859
+            day: 'Fri',
+            total: 5
         }, {
-            d: '2012-10-07',
-            visits: 790
-        }, {
-            d: '2012-10-08',
-            visits: 1680
-        }, {
-            d: '2012-10-09',
-            visits: 1592
-        }, {
-            d: '2012-10-10',
-            visits: 1420
-        }, {
-            d: '2012-10-11',
-            visits: 882
-        }, {
-            d: '2012-10-12',
-            visits: 889
-        }, {
-            d: '2012-10-13',
-            visits: 819
-        }, {
-            d: '2012-10-14',
-            visits: 849
-        }, {
-            d: '2012-10-15',
-            visits: 870
-        }, {
-            d: '2012-10-16',
-            visits: 1063
-        }, {
-            d: '2012-10-17',
-            visits: 1192
-        }, {
-            d: '2012-10-18',
-            visits: 1224
-        }, {
-            d: '2012-10-19',
-            visits: 1329
-        }, {
-            d: '2012-10-20',
-            visits: 1329
-        }, {
-            d: '2012-10-21',
-            visits: 1239
-        }, {
-            d: '2012-10-22',
-            visits: 1190
-        }, {
-            d: '2012-10-23',
-            visits: 1312
-        }, {
-            d: '2012-10-24',
-            visits: 1293
-        }, {
-            d: '2012-10-25',
-            visits: 1283
-        }, {
-            d: '2012-10-26',
-            visits: 1248
-        }, {
-            d: '2012-10-27',
-            visits: 1323
-        }, {
-            d: '2012-10-28',
-            visits: 1390
-        }, {
-            d: '2012-10-29',
-            visits: 1420
-        }, {
-            d: '2012-10-30',
-            visits: 1529
-        }, {
-            d: '2012-10-31',
-            visits: 1892
-        }, ],
-        // The name of the data record attribute that contains x-visitss.
-        xkey: 'd',
-        // A list of names of data record attributes that contain y-visitss.
-        ykeys: ['visits'],
-        // Labels for the ykeys -- will be displayed when you hover over the
-        // chart.
-        labels: ['Visits'],
-        // Disables line smoothing
-        smooth: false,
+            day: 'Sat',
+            total: 10
+        }],
+        xkey: 'day',
+        ykeys: ['total'],
+        labels: ['Total Amount In Purchases'],
+        xLabelAngle: 35,
+        hideHover: 'auto',
+        barColors: ['green'],
         resize: true
     });
+
+    // Get Deposits
+    $.get("http://api.reimaginebanking.com/accounts/560f0207f8d8770df0efa563/deposits?key=18cdc5d0c87e95621c50a23782e41559", function(data) { 
+        var length = data.length;
+        $(".dcount").text(length);
+
+        var j = 0;
+        for (var i = data.length - 1; j < 8; i--) {
+            var date = data[i]["transaction_date"].substring(0, 10);
+            var time = data[i]["transaction_date"].substring(11, 19);
+            var amount = data[i]["amount"];
+            var line = "<tr><td>" + date + "</td><td>" + time + "</td><td>" + amount + "</td></tr>"
+            $(".panel-dep tbody").append(line);
+            j++;
+        }
+
+        for (var i = 0; i < length; i++) {
+            var purchase = data[i];
+
+            var currentDate = new Date(purchase['transaction_date'].substring(0, 19));
+            var day = currentDate.getDay();
+            weeklyTotalsDeposits[day]["total"] += purchase["amount"];
+        }
+
+        myDepositBar.setData([{
+            day: weeklyTotalsDeposits[0]["day"],
+            total: weeklyTotalsDeposits[0]["total"]
+        }, {
+            day: weeklyTotalsDeposits[1]["day"],
+            total: weeklyTotalsDeposits[1]["total"]
+        }, {
+            day: weeklyTotalsDeposits[2]["day"],
+            total: weeklyTotalsDeposits[2]["total"]
+        }, {
+            day: weeklyTotalsDeposits[3]["day"],
+            total: weeklyTotalsDeposits[3]["total"]
+        }, {
+            day: weeklyTotalsDeposits[4]["day"],
+            total: weeklyTotalsDeposits[4]["total"]
+        }, {
+            day: weeklyTotalsDeposits[5]["day"],
+            total: weeklyTotalsDeposits[5]["total"]
+        }, {
+            day: weeklyTotalsDeposits[6]["day"],
+            total: weeklyTotalsDeposits[6]["total"]
+        }]);
+    });
+
+
+
+
+    
+
+
+
+
+
+
+    // Area Chart
+    // Morris.Area({
+    //     element: 'morris-area-chart',
+    //     data: [{
+    //         period: '2010 Q1',
+    //         iphone: 2666,
+    //         ipad: null,
+    //         itouch: 2647
+    //     }, {
+    //         period: '2010 Q2',
+    //         iphone: 2778,
+    //         ipad: 2294,
+    //         itouch: 2441
+    //     }, {
+    //         period: '2010 Q3',
+    //         iphone: 4912,
+    //         ipad: 1969,
+    //         itouch: 2501
+    //     }, {
+    //         period: '2010 Q4',
+    //         iphone: 3767,
+    //         ipad: 3597,
+    //         itouch: 5689
+    //     }, {
+    //         period: '2011 Q1',
+    //         iphone: 6810,
+    //         ipad: 1914,
+    //         itouch: 2293
+    //     }, {
+    //         period: '2011 Q2',
+    //         iphone: 5670,
+    //         ipad: 4293,
+    //         itouch: 1881
+    //     }, {
+    //         period: '2011 Q3',
+    //         iphone: 4820,
+    //         ipad: 3795,
+    //         itouch: 1588
+    //     }, {
+    //         period: '2011 Q4',
+    //         iphone: 15073,
+    //         ipad: 5967,
+    //         itouch: 5175
+    //     }, {
+    //         period: '2012 Q1',
+    //         iphone: 10687,
+    //         ipad: 4460,
+    //         itouch: 2028
+    //     }, {
+    //         period: '2012 Q2',
+    //         iphone: 8432,
+    //         ipad: 5713,
+    //         itouch: 1791
+    //     }],
+    //     xkey: 'period',
+    //     ykeys: ['iphone', 'ipad', 'itouch'],
+    //     labels: ['iPhone', 'iPad', 'iPod Touch'],
+    //     pointSize: 2,
+    //     hideHover: 'auto',
+    //     resize: true
+    // });
+
+    // Line Chart
+    // Morris.Line({
+    //     // ID of the element in which to draw the chart.
+    //     element: 'morris-line-chart',
+    //     // Chart data records -- each entry in this array corresponds to a point on
+    //     // the chart.
+    //     data: [{
+    //         d: '2012-10-01',
+    //         visits: 802
+    //     }, {
+    //         d: '2012-10-02',
+    //         visits: 783
+    //     }, {
+    //         d: '2012-10-03',
+    //         visits: 820
+    //     }, {
+    //         d: '2012-10-04',
+    //         visits: 839
+    //     }, {
+    //         d: '2012-10-05',
+    //         visits: 792
+    //     }, {
+    //         d: '2012-10-06',
+    //         visits: 859
+    //     }, {
+    //         d: '2012-10-07',
+    //         visits: 790
+    //     }, {
+    //         d: '2012-10-08',
+    //         visits: 1680
+    //     }, {
+    //         d: '2012-10-09',
+    //         visits: 1592
+    //     }, {
+    //         d: '2012-10-10',
+    //         visits: 1420
+    //     }, {
+    //         d: '2012-10-11',
+    //         visits: 882
+    //     }, {
+    //         d: '2012-10-12',
+    //         visits: 889
+    //     }, {
+    //         d: '2012-10-13',
+    //         visits: 819
+    //     }, {
+    //         d: '2012-10-14',
+    //         visits: 849
+    //     }, {
+    //         d: '2012-10-15',
+    //         visits: 870
+    //     }, {
+    //         d: '2012-10-16',
+    //         visits: 1063
+    //     }, {
+    //         d: '2012-10-17',
+    //         visits: 1192
+    //     }, {
+    //         d: '2012-10-18',
+    //         visits: 1224
+    //     }, {
+    //         d: '2012-10-19',
+    //         visits: 1329
+    //     }, {
+    //         d: '2012-10-20',
+    //         visits: 1329
+    //     }, {
+    //         d: '2012-10-21',
+    //         visits: 1239
+    //     }, {
+    //         d: '2012-10-22',
+    //         visits: 1190
+    //     }, {
+    //         d: '2012-10-23',
+    //         visits: 1312
+    //     }, {
+    //         d: '2012-10-24',
+    //         visits: 1293
+    //     }, {
+    //         d: '2012-10-25',
+    //         visits: 1283
+    //     }, {
+    //         d: '2012-10-26',
+    //         visits: 1248
+    //     }, {
+    //         d: '2012-10-27',
+    //         visits: 1323
+    //     }, {
+    //         d: '2012-10-28',
+    //         visits: 1390
+    //     }, {
+    //         d: '2012-10-29',
+    //         visits: 1420
+    //     }, {
+    //         d: '2012-10-30',
+    //         visits: 1529
+    //     }, {
+    //         d: '2012-10-31',
+    //         visits: 1892
+    //     }, ],
+    //     // The name of the data record attribute that contains x-visitss.
+    //     xkey: 'd',
+    //     // A list of names of data record attributes that contain y-visitss.
+    //     ykeys: ['visits'],
+    //     // Labels for the ykeys -- will be displayed when you hover over the
+    //     // chart.
+    //     labels: ['Visits'],
+    //     // Disables line smoothing
+    //     smooth: false,
+    //     resize: true
+    // });
 
 });
